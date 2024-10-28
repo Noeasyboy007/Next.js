11d8d10f4657c28dc7cc8d355556d369dc075f22
beraaritra <aritra.bera@lawsikho.in>
2024-10-28 17:31:09 +0530

    create pages

---
+++ src/app/info/page.js
@@ -0,0 +1,5 @@
+export default function info(){
+    return(
+        <h1>This is info component</h1>
+    )
+}
--- src/components/navbar/Header.js
+++ src/components/navbar/Header.js
@@ -1,17 +1,31 @@
 import Link from 'next/link';
-import styles from './Header.module.css';
 
 const Header = () => {
   return (
-    <header className={styles.header}>
-      <nav className={styles.nav}>
-        <ul>
-          <li><Link href="/">Home</Link></li>
-          <li><Link href="/about">About</Link></li>
-          <li><Link href="/blog">Blog</Link></li>
+
+    <nav className="bg-green-500 h-12 py-2 px-6 flex justify-between items-center" >
+      <div className='Heading'>
+        <h1 className='text-2xl font-semibold text-white'>
+        <a href={"/"}>Work Manager</a>
+        </h1>
+      </div>
+
+      <div>
+        <ul className='flex space-x-4 text-xl font-semibold text-white'>
+          <li> <a href="#">Home</a></li>
+          <li> <a href="#">Add Task</a></li>
+          <li><a href="#">Show Task</a> </li>
         </ul>
-      </nav>
-    </header>
+      </div>
+
+      <div>
+        <ul className='flex space-x-3 text-xl font-semibold text-white'>
+          <li><a href="#">Login</a></li>
+          <li><a href="#">Signup</a></li>
+        </ul>
+      </div>
+    </nav >
+
   );
 };
 
